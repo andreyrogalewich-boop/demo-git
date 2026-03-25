6b9e657151efee5ca096971346d9abfbb2fae3fc
andreyrogalewich-boop <andreyrogalewich@gmail.com>
2026-03-25 21:37:11 +0300

    Update Main.java
    refactor name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     static void main(String[] args) {
         System.out.println("HEllo World");
-         System.out.println("Bye-bye World");
+         System.out.println("Bye-bye iryna");
     }
 }
